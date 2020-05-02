433765974b34203fea178c50747eca498d6e71e7
ashrasmun <ashrasmun@gmail.com>
2020-05-02 09:35:05 +0200

    Rewrites the main so that it uses Windows API
    The WinAPI will be needed for future uses of the project, so there needs
    to be some way of starting the project. The `add_compile_options` is
    taken straight from CMake's documentation - it sets flags for Unix
    systems, but as of now, I do not plan to create this application for
    Unix systems. I will try to do it after I'm done with at least one
    platform :)

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+#include <windows.h>
 
-int main()
+int CALLBACK
+WinMain(HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        LPSTR lpCmdLine,
+        int nCmdShow)
 {
-    std::cout << "Helloo";
+    /* TODO: Fix the TODO snippet <01-05-20, ashrasmun> */
+    /* TODO: change the todo snippet, so that the nick in on the right of comma <01-05-20, ashrasmun> */
+    /* TODO: add syntax constraints same to python ones in vimrc <01-05-20, ashrasmun> */
+    UNREFERENCED_PARAMETER(hInstance);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nCmdShow);
+
+    MessageBox(0, "Hello :)", "I work!", MB_OK);
     return 0;
 }
