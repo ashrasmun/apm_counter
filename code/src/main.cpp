0264dce1928c44cae7e9bc55e9c048927dccf737
ashrasmun <ashrasmun@gmail.com>
2020-05-07 20:11:06 +0200

    Add basic Windows window and message handling
    Most probably the window will disappear in the future, but given that
    I'm learning more than I develop at the moment, I don't bother with
    having such a commit in this repo. I first need to get used to message
    handling by Windows and then I can move to cleaning whole application
    up.

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,19 +1,104 @@
 #include <windows.h>
+#include <iostream>
+
+LRESULT CALLBACK
+MainWindowCallback(_In_ HWND   hwnd,
+                   _In_ UINT   message,
+                   _In_ WPARAM wparam,
+                   _In_ LPARAM lparam)
+{
+    LRESULT result = 0;
+
+    switch (message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+        case WM_CLOSE:
+        {
+            DestroyWindow(hwnd);
+        } break;
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+        case WM_LBUTTONDOWN:
+        {
+            MessageBoxA(0, "message", "title", MB_OK);
+        } break;
+        default:
+        {
+            result = DefWindowProcA(hwnd, message, wparam, lparam);
+        } break;
+    }
+
+    return result;
+}
 
 int CALLBACK
-WinMain(HINSTANCE hInstance,
-        HINSTANCE hPrevInstance,
-        LPSTR lpCmdLine,
-        int nCmdShow)
+WinMain(_In_     HINSTANCE instance,
+        _In_opt_ HINSTANCE previous_instance,
+        _In_     LPSTR     cmd_line,
+        _In_     int       cmd_show)
 {
-    /* TODO: Fix the TODO snippet <01-05-20, ashrasmun> */
-    /* TODO: change the todo snippet, so that the nick in on the right of comma <01-05-20, ashrasmun> */
-    /* TODO: add syntax constraints same to python ones in vimrc <01-05-20, ashrasmun> */
-    UNREFERENCED_PARAMETER(hInstance);
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-    UNREFERENCED_PARAMETER(nCmdShow);
+    UNREFERENCED_PARAMETER(previous_instance);
+    UNREFERENCED_PARAMETER(cmd_line);
+    UNREFERENCED_PARAMETER(cmd_show);
 
-    MessageBox(0, "Hello :)", "I work!", MB_OK);
-    return 0;
+    WNDCLASS window = {};
+
+    window.style         = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    window.lpfnWndProc   = MainWindowCallback;
+    window.hInstance     = instance;
+    window.lpszClassName = "MainWindowClass";
+
+    if (!RegisterClassA(&window))
+        return 1;
+
+    HWND handle = CreateWindowExA(
+            0,
+            window.lpszClassName,
+            "apm_counter",
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            0,
+            0,
+            instance,
+            0);
+
+    if (!handle)
+        return 1;
+
+    MSG message;
+    BOOL result;
+
+    while (true)
+    {
+        result = GetMessageA(&message, 0, 0, 0);
+
+        if (result > 0)
+        {
+            TranslateMessage(&message);
+            DispatchMessageA(&message);
+        }
+        else if (result < 0)
+        {
+            // error
+        }
+        else
+        {
+            // result == 0 means the exit from the program
+            break;
+        }
+    }
+
+    return message.wParam;
 }
